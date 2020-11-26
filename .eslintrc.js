a650fa3fb4851e38cc75c025e535ac1719740948
Pawel Mucha <pawel@mucha.com>
2020-11-26 10:44:24 +0100

    Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,25 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/essential",
+    "@vue/standard",
+    "eslint:recommended"
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
+    ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "indent": ["warn", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+    "semi": ["error", "never"],
+    "no-nested-ternary": "error",
+    "no-unneeded-ternary": "error",
+    "no-multiple-empty-lines": ["error", { "max": 2 }],
+    "yoda": "error",
+    "comma-dangle": ["error", "always-multiline"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === 'production' ? "warn" : "off"
   }
 }
