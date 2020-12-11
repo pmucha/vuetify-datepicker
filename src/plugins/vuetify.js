aae070941d41adc09f8056534e4d3cff94f6ab3a
Pawel Mucha <pawel@mucha.com>
2020-12-11 15:32:21 +0100

    Added support for SVG version of Material Icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,17 @@
 import Vue from "vue"
 import Vuetify from "vuetify/lib/framework"
-import "@mdi/font/css/materialdesignicons.css"
+// Turn on standard mdi fonts:
+// import "@mdi/font/css/materialdesignicons.css"
+
+// Turn on mdiSvg fonts
+// import { ... } from "@mdi/js"
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  title: "Vuetify Date Picker",
   icons: {
-    iconfont: "mdi",
+    // iconfont: "mdi",
+    iconfont: "mdiSvg",
   },
 })
